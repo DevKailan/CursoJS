3829a9c02c9077d044cd02cfb8629c386c8d9f2b
Kailanscea <kailanscea@gmail.com>
2025-03-16 12:33:39 -0300

    Conteúdo  aula07

---
+++ aula07/index.js
@@ -0,0 +1,6 @@
+//String = Texto | Number = Número
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
