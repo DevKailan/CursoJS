85a32dc825d7bc446c57d93efb4671c6f2f20515
Kailanscea <kailanscea@gmail.com>
2025-03-16 14:38:04 -0300

    Aula 09

---
+++ aula09/index.js
@@ -0,0 +1,13 @@
+//String, number, undefined, null, boolean
+const nome = 'Luiz' // String
+const nome1 = 'Luiz' // String
+const nome2 = 'Luiz' // String
+const num1 = 10; // number
+const num2 = 10.82 // number
+
+let nomeAluno; // undefined -> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhum  na memória
+const aprovado = false; // Boolean = true, false (lógico)
+
+let a = 2;
+const b = a;
